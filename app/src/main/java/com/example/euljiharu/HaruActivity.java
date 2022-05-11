a65c61c4ec0db7184864eea94b0052f9d0953e27
uoehoswi3 <thdus1821@naver.com>
2022-05-11 15:49:28 +0900

    fourth commit

---
+++ app/src/main/java/com/example/euljiharu/HaruActivity.java
@@ -0,0 +1,15 @@
+package com.example.euljiharu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class HaruActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_haru);
+    }
+}
--- app/src/main/java/com/example/euljiharu/MainActivity.java
+++ app/src/main/java/com/example/euljiharu/MainActivity.java
@@ -2,15 +2,39 @@ package com.example.euljiharu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button EuljiButton, HaruButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EuljiButton = findViewById(R.id.btn_eulji);
+        HaruButton = findViewById(R.id.btn_haru);
+
+        EuljiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, EuljiActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        HaruButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, HaruActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private long backKeyPressedTime = 0;
